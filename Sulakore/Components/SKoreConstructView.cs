d66c62ff3ef6fdc48586d1f0fa1544473f8d98ca
misdadiger <mod@null.net>
2016-06-28 16:11:42 +0200

    First commit. ArachisH quit, so here we go, doing our best to continue what he made :)

---
+++ Sulakore/Components/SKoreConstructView.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+using Sulakore.Protocol;
+
+namespace Sulakore.Components
+{
+    [DesignerCategory("Code")]
+    public class SKoreConstructView : SKoreListView
+    {
+        private HMessage _cachedPacket;
+        private readonly List<object> _valuesWritten;
+
+        public IReadOnlyList<object> ValuesWritten { get; }
+
+        public SKoreConstructView()
+        {
+            _valuesWritten = new List<object>();
+            ValuesWritten = _valuesWritten.AsReadOnly();
+        }
+
+        public void WriteInteger(int value)
+        {
+            WriteInteger(value, 1);
+        }
+        public void WriteInteger(int value, int amount)
+        {
+            WriteValue("Integer", value,
+                BigEndian.GetBytes(value), amount);
+        }
+
+        public void WriteBoolean(bool value)
+        {
+            WriteBoolean(value, 1);
+        }
+        public void WriteBoolean(bool value, int amount)
+        {
+            WriteValue("Boolean", value,
+                BigEndian.GetBytes(value), amount);
+        }
+
+        public void WriteString(string value)
+        {
+            WriteString(value, 1);
+        }
+        public void WriteString(string value, int amount)
+        {
+            WriteValue("String", value,
+                BigEndian.GetBytes(value), amount);
+        }
+
+        protected virtual void MoveValue(int oldIndex, int newIndex)
+        {
+            lock (_valuesWritten)
+            {
+                object value = _valuesWritten[oldIndex];
+                _valuesWritten.RemoveAt(oldIndex);
+                _valuesWritten.Insert(newIndex, value);
+                _cachedPacket = null;
+            }
+        }
+        protected virtual void WriteValue(string type, object value, byte[] data, int amount)
+        {
+            lock (_valuesWritten)
+            {
+                BeginUpdate();
+                string encoded = HMessage.ToString(data);
+                for (int i = 0; i < amount; i++)
+                {
+                    _valuesWritten.Add(value);
+                    ListViewItem item = null;
+                    if (i != (amount - 1))
+                    {
+                        item = AddItem(type, value, encoded);
+                    }
+                    else item = AddFocusedItem(type, value, encoded);
+                }
+                EndUpdate();
+                _cachedPacket = null;
+            }
+        }
+
+        protected override void MoveItemUp(ListViewItem item)
+        {
+            int oldIndex = item.Index;
+            base.MoveItemUp(item);
+            MoveValue(oldIndex, item.Index);
+        }
+        protected override void MoveItemDown(ListViewItem item)
+        {
+            int oldIndex = item.Index;
+            base.MoveItemDown(item);
+            MoveValue(oldIndex, item.Index);
+        }
+        protected override void RemoveItem(ListViewItem item, bool selectNext)
+        {
+            lock (_valuesWritten)
+            {
+                _valuesWritten.RemoveAt(item.Index);
+                base.RemoveItem(item, selectNext);
+                _cachedPacket = null;
+            }
+        }
+
+        public void UpdateSelectedValue(object value)
+        {
+            if (HasSelectedItem)
+                UpdateValue(SelectedItem, value);
+        }
+        protected virtual void UpdateValue(ListViewItem item, object value)
+        {
+            lock (_valuesWritten)
+            {
+                byte[] data = null;
+                switch (item.SubItems[0].Text)
+                {
+                    case "String": data = BigEndian.GetBytes((string)value); break;
+                    case "Integer": data = BigEndian.GetBytes((int)value); break;
+                    case "Boolean": data = BigEndian.GetBytes((bool)value); break;
+                }
+
+                string encoded = HMessage.ToString(data);
+                item.SubItems[1].Text = value.ToString();
+                item.SubItems[2].Text = encoded;
+
+                _valuesWritten[item.Index] = value;
+                _cachedPacket = null;
+            }
+        }
+
+        public HMessage GetPacket(ushort header)
+        {
+            lock (_valuesWritten)
+            {
+                if (_cachedPacket == null)
+                {
+                    _cachedPacket =
+                        new HMessage(header, _valuesWritten.ToArray());
+                }
+                _cachedPacket.Header = header;
+                return _cachedPacket;
+            }
+        }
+        public string GetStructure(ushort header)
+        {
+            lock (_valuesWritten)
+            {
+                string structure = $"{{l}}{{u:{header}}}";
+                foreach (object value in _valuesWritten)
+                {
+                    char type = Type.GetTypeCode(
+                        value.GetType()).ToString().ToLower()[0];
+
+                    structure += $"{{{type}:{value}}}";
+                }
+                return structure;
+            }
+        }
+    }
+}
---
+++ Sulakore/Components/SKoreLabel.cs
@@ -0,0 +1,151 @@
+﻿using System.Timers;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace Sulakore.Components
+{
+    [DesignerCategory("Code")]
+    public class SKoreLabel : Control
+    {
+        private int _dotCount;
+        private string _frame;
+        private readonly System.Timers.Timer _timer;
+
+        private int _borderWidth = 1;
+        [DefaultValue(1)]
+        public int BorderWidth
+        {
+            get { return _borderWidth; }
+            set { _borderWidth = value; Invalidate(); }
+        }
+
+        private bool _isBorderVisible = true;
+        [DefaultValue(true)]
+        public bool IsBorderVisible
+        {
+            get { return _isBorderVisible; }
+            set { _isBorderVisible = value; Invalidate(); }
+        }
+
+        private Color _skin = Color.SteelBlue;
+        [DefaultValue(typeof(Color), "SteelBlue")]
+        public Color Skin
+        {
+            get { return _skin; }
+            set { _skin = value; Invalidate(); }
+        }
+
+        [DefaultValue(typeof(Color), "White")]
+        public override Color BackColor
+        {
+            get { return base.BackColor; }
+            set { base.BackColor = value; }
+        }
+
+        [DefaultValue((double)250)]
+        public double Interval
+        {
+            get { return _timer.Interval; }
+            set { _timer.Interval = value; }
+        }
+
+        [Browsable(false)]
+        [DefaultValue(false)]
+        public bool IsAnimating
+        {
+            get { return _timer.Enabled; }
+            set
+            {
+                if (_timer.Enabled == value)
+                    return;
+
+                if (value)
+                {
+                    _dotCount = 1;
+                    _frame = (BaseAnimation + ".");
+                }
+                else _frame = string.Empty;
+
+                _timer.Enabled = value;
+                Invalidate();
+            }
+        }
+
+        [DefaultValue(3)]
+        public int MaximumDots { get; set; } = 3;
+
+        private string _baseAnimation = "Working";
+        [DefaultValue("Working")]
+        public string BaseAnimation
+        {
+            get { return _baseAnimation; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    value = "Working";
+
+                if (_baseAnimation == value)
+                    return;
+
+                _baseAnimation = value;
+                if (IsAnimating)
+                {
+                    _timer.Enabled = false;
+                    IsAnimating = true;
+                }
+            }
+        }
+
+        public SKoreLabel()
+        {
+            SetStyle((ControlStyles)2050, true);
+            BackColor = Color.White;
+            DoubleBuffered = true;
+
+            Height = 20;
+
+            _timer = new System.Timers.Timer(250);
+            _timer.SynchronizingObject = this;
+            _timer.Elapsed += Elapsed;
+        }
+
+        private void Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (_dotCount >= MaximumDots)
+            {
+                _dotCount = 0;
+                _frame = BaseAnimation;
+            }
+            _frame += ".";
+            _dotCount++;
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.Clear(BackColor);
+            if (IsBorderVisible)
+            {
+                using (var brush = new SolidBrush(Skin))
+                {
+                    e.Graphics.FillRectangle(brush, 0, 0, BorderWidth, Height);
+                    e.Graphics.FillRectangle(brush, Width - BorderWidth, 0, BorderWidth, Height);
+
+                    TextRenderer.DrawText(e.Graphics,
+                        (IsAnimating ? _frame : Text), Font, ClientRectangle, ForeColor);
+                }
+            }
+            base.OnPaint(e);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!IsDisposed && disposing)
+            {
+                _timer.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
